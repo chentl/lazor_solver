5eded3682d284a2a0e303ca0a3fbd7fce0d22691
halghm4 <47227346+halghm4@users.noreply.github.com>
2019-04-06 18:26:34 -0400

    Initial creation of lazor.py
    Initial running code for loading and solving a board of Lazors
    
    to be added:
       * image board loader
       * image board exporter
    
    comments:
       * comprehensive docstrings will be added once the code is complete
       * cleaning would be done after most of the code is implemented

---
+++ lazor.py
@@ -0,0 +1,479 @@
+
+from itertools import combinations, product
+from operator import itemgetter
+from math import factorial
+import numpy as np
+import random
+import time
+import os
+
+'''
+Lazor Project
+
+**Functions**
+
+        lazor_load_bff: 
+            load bff file to be read and manipulated by other functions
+
+        lazor_solve:
+            function to solve a loaded board by iterating through possible combinations
+
+        get_possible_combs_perm:
+            obtain all possible combinations for moveable blocks in available positions
+
+        get_available_positions:
+            obtain all available positions for block placement
+
+        get_data_grid:
+            convert letter_grid (loaded by lazor_load_bff) to data_grid
+
+        lazor_on:
+            function to switch lazers on and trace thier positions
+
+        pos_chk:
+            positions checker to confirm point is in grid
+
+**bff file info**
+        Grid: either one of the following, must start and end with 
+            flags GRID START, and GRID STOP
+          x = no block allowed [0]
+          o = blocks allowed [0]
+          A = fixed reflect block [2]
+          B = fixed opaque block [4]
+          C = fixed refract block [6]
+            example:
+                GRID START
+                o   o   o   o   o
+                o   o   o   o   o
+                o   o   o   o   x
+                o   o   o   o   o
+                o   o   o   o   o
+                GRID STOP
+        Blocks:
+          A = reflect block [2]
+          B = opaque block [4]
+          C = refract block [6]
+
+        Lazers: x, y, vx, vy [8]
+          L 2 7 1 -1
+
+        Target Points: x, y [9]
+          P 0 1
+
+**Examples of data representation in run**
+
+        letter_grid = [['o' 'o' 'A' 'o' 'o']
+                       ['o' 'o' 'o' 'A' 'o']
+                       ['A' 'o' 'A' 'o' 'x']
+                       ['o' 'o' 'o' 'A' 'o']
+                       ['o' 'o' 'A' 'o' 'o']]
+
+        blocks = ['A', 'A', 'A', 'A', 'A', 'A']
+
+        lazers = [['2', '1', '1', '1'], ['9', '4', '-1', '1']]
+
+        points = [['6', '3'], ['6', '5'], ['6', '7'], ['2', '9'], ['9', '6']]
+
+        available_positions = [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (0, 1),
+                               (1, 1), (2, 1), (3, 1), (4, 1), (0, 2), (1, 2),
+                               (2, 2), (3, 2), (0, 3), (1, 3), (2, 3), (3, 3),
+                               (4, 3), (0, 4), (1, 4), (2, 4), (3, 4), (4, 4)]
+
+        data_grid = [[ 0.  0.  0.  0.  2.  2.  2.  0.  0.  0.  0.]
+                     [ 0.  0.  8.  0.  2.  2.  2.  0.  0.  0.  0.]
+                     [ 0.  0.  0.  8.  2.  2.  2.  2.  2.  0.  0.]
+                     [ 0.  0.  0.  0.  8.  0. -1.  2.  2.  0.  0.]
+                     [ 2.  2.  2.  8.  2.  2.  2.  2.  2.  8.  0.]
+                     [ 2.  2.  2.  0.  2.  2. -1.  0.  8.  0.  0.]
+                     [ 2.  2.  2.  8.  2.  2.  2.  2.  2. -1.  0.]
+                     [ 0.  0.  0.  0.  8.  0. -1.  2.  2.  0.  8.]
+                     [ 0.  0.  0.  8.  2.  2.  2.  2.  2.  0.  0.]
+                     [ 0.  0. -1.  0.  2.  2.  2.  0.  0.  0.  0.]
+                     [ 0.  8.  0.  0.  2.  2.  2.  0.  0.  0.  0.]]
+
+        possible_combs = [[('A', (0, 0)), ('A', (1, 0)), ('A', (2, 0)), ('A', (3, 0)), ('A', (4, 0)), ('A', (0, 1))],
+                          [('A', (0, 0)), ('A', (1, 0)), ('A', (2, 0)), ('A', (3, 0)), ('A', (4, 0)), ('A', (1, 1))],
+                          [('A', (0, 0)), ('A', (1, 0)), ('A', (2, 0)), ('A', (3, 0)), ('A', (4, 0)), ('A', (2, 1))],
+                          ....
+                          [('A', (4, 3)), ('A', (0, 4)), ('A', (1, 4)), ('A', (2, 4)), ('A', (3, 4)), ('A', (4, 4))]]
+            
+
+'''
+
+
+def lazor_load_bff(inp):
+    '''
+    Load lazor data from a bff file
+
+    **Parameters**
+
+        inp: *string*
+            path to bff file to be read and parsed
+
+    **Returns**
+
+        valid: *np.array*, *list*, *list*, *list*, *list*
+            a numpy array of the grid in letters, and lists of movealbe blocks,
+            lazer positions and directions, and target points positions, and
+            the number of each block [A, B, C]
+    '''
+    letter_grid, blocks, lazers, points, A, B, C = [], [], [], [], 0, 0, 0
+
+    with open(inp) as lazor_file:
+        grid_switch = False
+        for line in lazor_file:
+            if line.strip() == "GRID START":
+                grid_switch = True
+            elif line.strip() == "GRID STOP":
+                grid_switch = False
+            elif grid_switch:
+                letter_grid.append(line.strip().split())
+            elif (line.strip()).split(" ")[0] == "A":
+                A = int((line.strip()).split(" ")[1])
+                for a in range(A):
+                    blocks.append('A')
+            elif (line.strip()).split(" ")[0] == "B":
+                B = int((line.strip()).split(" ")[1])
+                for b in range(B):
+                    blocks.append('B')
+            elif (line.strip()).split(" ")[0] == "C":
+                C = int((line.strip()).split(" ")[1])
+                for c in range(C):
+                    blocks.append('C')
+            elif (line.strip()).split(" ")[0] == "L":
+                lazers.append((line.strip()).split(" ")[1:])
+            elif (line.strip()).split(" ")[0] == "P":
+                points.append((line.strip()).split(" ")[1:])
+    letter_grid = np.array(letter_grid)
+
+    return letter_grid, blocks, lazers, points, [A, B, C]
+
+
+def lazor_solve(data):
+    '''
+    lazor (game) solver
+
+    **Parameters**
+
+        data: *list* {*np.array*, *list*, *list*, *list*}
+            numpy array of letter_grid, lists of movealbe blocks letters,
+            lazer positions and directions, and target points positions
+
+    **Returns**
+
+        valid: *np.array*
+            data array for the solved maze
+    '''
+
+    # load data and count number of available positions
+    letter_grid, blocks, lazers, points, block_counts = data
+    available_positions = get_available_positions(letter_grid)
+
+    # calculate the number of moveable blocks and available positions [debug]
+    n_blocks, n_pos = len(blocks), len(available_positions)
+    # print the number of unique combinations
+    print(factorial(n_pos) / (factorial(n_pos - n_blocks) * factorial(block_counts[0]) * factorial(block_counts[1]) * factorial(block_counts[2])))
+
+    unique_blocks = 0
+
+    for bc in block_counts:
+        if bc > 0:
+            unique_blocks += 1
+
+    # print(unique_blocks)
+
+    # if the number of block types is 1, user combinations, otherwise use the modified functions
+    if unique_blocks == 1:
+        t0 = time.time()
+        possible_combs = [zip(blocks, x) for x in combinations(available_positions, len(blocks))]
+        print("%i permutations generated in %.3f s" % (len(possible_combs), time.time() - t0))
+    elif unique_blocks > 1:
+        t0 = time.time()
+        # possible_combs = [zip(blocks, x) for x in combinations(available_positions, len(blocks))]
+        possible_combs = get_possible_combs_perm(blocks, available_positions)
+        print("%i permutations generated in %.3f s" % (len(possible_combs), time.time() - t0))
+
+    c = 1
+    # Iterate a random combination each time and turn lazor on
+    for comb_number in range(len(possible_combs)):
+        comb_ind = random.randint(0, len(possible_combs) - 1)
+        comb = possible_combs.pop(comb_ind)
+
+        i_grid = []
+        i_grid = letter_grid.copy()
+
+        for blk in comb:
+            i_grid[blk[1][1]][blk[1][0]] = blk[0]
+
+        data_grid = get_data_grid(i_grid, points)
+        data_grid_w_lazer_on = lazor_on(data_grid, lazers)
+        # print(i_grid)
+        # print(data_grid_w_lazer_on)
+
+        # stop if solution is found [if no 9 is in data_grid]
+        if not any([point == 9 for row in data_grid_w_lazer_on for point in row]):
+            print("Solution found in %i iterations" % (c))
+            return i_grid
+        c += 1
+
+    print("No solution found!")
+
+
+def get_possible_combs_perm(blocks, available_positions):
+    '''
+    unique combinations generator. This function still generates similar
+    combinations
+
+    **Parameters**
+
+        blocks: *list*
+            list of moveable blocks letters
+
+        available_positions: *list*
+            list of (x, y) coordinates of available positions
+
+    **Returns**
+
+        possible_combs: *list*{*list*{*string*, *tulpe*}}
+            list of all possible combinations of letters in available positions
+    '''
+    num_avail_blocks = len(blocks)
+
+    i_perm = []
+    for i in range(num_avail_blocks):
+        i_comb = [blocks[i], available_positions]
+        i_perm.append(product(* i_comb))
+
+    j_perm = product(* i_perm)
+    j_perm = {frozenset(j) for j in j_perm}
+    j_perm = [list(i) for i in j_perm]
+
+    possible_combs = []
+
+    for comb in j_perm:
+        if len(comb) == num_avail_blocks:
+            comb = sorted(comb, key=itemgetter(0, 1))
+            if comb not in possible_combs:
+                possible_combs.append(comb)
+                # print(comb)
+
+    return possible_combs
+
+
+def get_available_positions(letter_grid):
+    '''
+    From a letter grid, this function extracts avilable positions for a
+    moveable block to be placed at.
+
+    **Parameters**
+
+        letter_grid: *np.array*
+            letter grid generated by lazor_load function
+
+    **Returns**
+
+        available_positions: *list, tuples, (x, y)*
+            a list of all avaiable positions in (x, y) coordinates
+    '''
+    available_positions = []
+    for j in range(len(letter_grid)):
+        for i in range(len(letter_grid[0])):
+            if letter_grid[j][i] == 'o':
+                available_positions.append((i, j))
+    return available_positions
+
+
+def get_data_grid(letter_grid, points):
+    '''
+    Convert letter grid into data grid and include target points
+
+    **Parameters**
+
+        letter_grid: *np.array*
+            letter grid generated by lazor_load function
+
+        points: *list*
+            list of target points obtained by lazor_load function
+
+    **Returns**
+
+        data_grid: *np.array*
+            converted data grid
+    '''
+    x_dim, y_dim = 2 * len(letter_grid) + 1, 2 * len(letter_grid[0]) + 1
+    data_grid = np.zeros(shape=(x_dim, y_dim))
+    directions = [
+        (-1, 0),
+        (1, 0),
+        (0, -1),
+        (0, 1),
+        (-1, -1),
+        (-1, 1),
+        (1, -1),
+        (1, 1)]
+
+    for j in range(len(letter_grid)):
+        for i in range(len(letter_grid[0])):
+            x, y = 2 * i + 1, 2 * j + 1
+            block = letter_grid[j][i]
+            neighbors = [(x + direction[0], y + direction[1]) for direction in directions]
+            if block == "A":
+                data_grid[y, x] = 2
+                for neighbor in neighbors:
+                    val = data_grid[neighbor[1], neighbor[0]]
+                    if val != 4:
+                        data_grid[neighbor[1], neighbor[0]] = 2
+            if block == "B":
+                data_grid[y, x] = 4
+                for neighbor in neighbors:
+                    data_grid[neighbor[1], neighbor[0]] = 4
+            if block == "C":
+                data_grid[y, x] = 6
+                for neighbor in neighbors:
+                    val = data_grid[neighbor[1], neighbor[0]]
+                    if (val != 4 or val != 2):
+                        data_grid[neighbor[1], neighbor[0]] = 6
+
+    for point in points:
+            data_grid[int(point[1]), int(point[0])] = 9
+
+    return data_grid
+
+
+def lazor_on(data_grid, lazers):
+    '''
+    Validate if the coordinates specified (x and y) are within the maze.
+
+    **Parameters**
+
+        x: *int*
+            An x coordinate to check if it resides within the maze.
+        y: *int*
+            A y coordinate to check if it resides within the maze.
+        nBlocks: *int*
+            How many blocks wide the maze is.  Should be equivalent to
+            the length of the maze (ie. len(maze)).
+
+    **Returns**
+
+        valid: *bool*
+            Whether the coordiantes are valid (True) or not (False).
+    '''
+    laz_grid = data_grid.copy()
+    lazs = list(lazers)
+    for lazer in lazs:
+        pos = [int(lazer[0]), int(lazer[1])]
+        lazer_vector = [int(lazer[2]), int(lazer[3])]
+
+        while True:
+            # determine if on side or t/b
+            # in each case, get the values of neighbors' center of blocks
+            # proceed according to the values (i.e. 2, 4, 6)
+            # where (2)     reflects to one direction
+            #       (4)     blocks the lazer
+            #       (6)     passes throght in its direction and relects in another
+            #                   (additional lazer must be generated???)
+
+            # loc = 0 (side of block), loc = 1 (top or bottom of block)
+            if not pos_chk(pos[0], pos[1], data_grid):
+                # break the while loop (turn off lazer) once exceeding the bound.
+                break
+            pos_val = data_grid[pos[1], pos[0]]
+            if pos_val == 9:
+                laz_grid[pos[1], pos[0]] = -1
+            elif pos_val == 0:
+                laz_grid[pos[1], pos[0]] = 8
+
+            if (pos[0] % 2) == 0:
+                # Side (check ONLY right or left neighbor based on direction)
+                reflection = 0
+                incident_pos = [pos[0] + lazer_vector[0], pos[1]]
+                other_pos = [pos[0] - lazer_vector[0], pos[1]]
+
+            elif (pos[1] % 2) == 0:
+                # Top or Bottom
+                reflection = 1
+                incident_pos = [pos[0], pos[1] + lazer_vector[1]]
+                other_pos = [pos[0], pos[1] - lazer_vector[1]]
+
+            if not pos_chk(incident_pos[0], incident_pos[1], data_grid):
+                # break the while loop (turn off lazer) once exceeding the bound.
+                break
+            incident_pos_val = data_grid[incident_pos[1], incident_pos[0]]
+
+            # check the position of other block if valid, and get its value or set 0
+            if pos_chk(other_pos[0], other_pos[1], data_grid):
+                other_pos_val = data_grid[other_pos[1], other_pos[0]]
+            else:
+                other_pos_val = 0
+
+            # if passable block, pass light to next position in direction
+            if incident_pos_val == 0:
+                pos = [pos[0] + lazer_vector[0], pos[1] + lazer_vector[1]]
+            # if reflection block, reflect lazer
+            elif incident_pos_val == 2:
+                # if lazer is trapped between two reflection blocks, stop lazer
+                if (other_pos_val == 2 or other_pos_val == 4):
+                    break
+                lazer_vector[reflection] = -1 * lazer_vector[reflection]
+                pos = [pos[0] + lazer_vector[0], pos[1] + lazer_vector[1]]
+            # if opaque block, stop lazer
+            elif incident_pos_val == 4:
+                break
+            # if refreact block, refract lazer
+            elif incident_pos_val == 6:
+                # if other block is reflection block, only pass through
+                if (other_pos_val == 2 or other_pos_val == 4):
+                    pos = [pos[0] + lazer_vector[0], pos[1] + lazer_vector[1]]
+                # otherwise, passthrough lazer, and add additional reflection lazer
+                else:
+                    lazer_vector_temp = list(lazer_vector)
+                    lazer_vector_temp[reflection] = -1 * lazer_vector_temp[reflection]
+                    lazs.append([pos[0], pos[1], lazer_vector_temp[0], lazer_vector_temp[1]])
+                    pos = [pos[0] + lazer_vector[0], pos[1] + lazer_vector[1]]
+
+    return laz_grid
+
+
+def pos_chk(x, y, data_grid):
+    '''
+    Validate if the coordinates specified (x and y) are within the maze.
+
+    **Parameters**
+
+        x: *int*
+            An x coordinate to check if it resides within the maze.
+        y: *int*
+            A y coordinate to check if it resides within the maze.
+        nBlocks: *int*
+            How many blocks wide the maze is.  Should be equivalent to
+            the length of the maze (ie. len(maze)).
+
+    **Returns**
+
+        valid: *bool*
+            Whether the coordiantes are valid (True) or not (False).
+    '''
+    # x_dim, y_dim = 2 * len(grid[0]) + 1, 2 * len(grid) + 1
+    x_dim, y_dim = len(data_grid[0, :]), len(data_grid[:, 0])
+    # print(x_dim, y_dim)
+    return x >= 0 and x < x_dim and y >= 0 and y < y_dim
+
+
+# Test and optimization utilities unit
+if __name__ == "__main__":
+    files = os.listdir('./bff')
+    for file in files:
+        print("-------------------------------------------")
+        fptr = os.path.join('bff', file)
+        data = lazor_load_bff(fptr)
+        trial_number = 1
+        durations = []
+        for i in range(trial_number):
+            # print(i)
+            t0 = time.time()
+            solution = lazor_solve(data)
+            durations.append(time.time() - t0)
+        # print(solution)
+        print('PERFORMANCE')
+        print('     Avg: %.3f, Low: %.3f, High: %.3f' % (sum(durations) / len(durations), min(durations), max(durations)))
